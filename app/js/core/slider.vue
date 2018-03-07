32586269168afb97d73d039a9d3bd3e013e6af2d
jiaen188 <244189981@qq.com>
2018-03-07 22:56:21 +0800

    基本组件btn ，slider，panel

---
+++ app/js/core/slider.vue
@@ -0,0 +1,52 @@
+<template>
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.noNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link to="{ name: item.href}">
+                    <img :src="item.src">
+                </router-link>
+            </swiper-slide>
+            <div class="swiper-pagination" v-if="options.pagination"></div>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+export default {
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {
+                    autoplay: true,
+                    loop: true,
+                    // 是否需要小圆点
+                    pagination: {
+                        el: ".swiper-pagination"
+                    },
+                    noNextTick: false
+                }
+            }
+        },
+        items: {
+            type: Array,
+            default() {
+                return [{
+                  href: "", // 点击图片要跳转的链接
+                  src: ""  // 图片的链接
+                }]
+            }
+        }
+    },
+    components: {
+        swiper,
+        swiperSlide
+    }
+}
+</script>
+
+<style lang="scss">
+@import "~swiper/dist/css/swiper.css"
+</style>
---
+++ app/js/public/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <span class="left">
+            <em>注册</em>&nbsp;|<em>登录</em>
+        </span>
+        <btn class="right">app下载</btn>
+    </div>
+</template>
+
+<script>
+import btn from '../core/btn.vue';
+
+export default {
+    components: {
+        btn
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
