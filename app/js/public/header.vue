c332ede27c3871c980feebba73fb4d955b8d1246
jiaen188 <244189981@qq.com>
2018-03-09 22:42:04 +0800

    首页的header和引入hotcss实现自适应

--- app/js/public/header.vue
+++ app/js/public/header.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="header">
-        <span class="left">
-            <em>注册</em>&nbsp;|<em>登录</em>
+    <div :class="$style.header">
+        <span :class="$style.left">
+            <em>注册</em>&nbsp;|&nbsp;<em>登录</em>
         </span>
-        <btn class="right">app下载</btn>
+        <btn :class="$style.btnDownload">app下载</btn>
     </div>
 </template>
 
@@ -17,6 +17,35 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" module>
+.header{
+    color: #666;
+    height: 100px;
+    line-height: 100px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    font-size: 32px;
+    background: #fff url(//m.jr.jd.com/spe/qyy/main/images/jr-logo.png) center center no-repeat;
+    background-size: auto 50%;
+    z-index: 100;
+    .left{
+        font-size: 28px;
+        height: 30px;
+        line-height: 30px;
+        margin: 17px 0 0 18px;
+    }
+    .btnDownload{
+        float: right;
+        font-size: 24px;
+        border-width: 0;
+        height: 56px;
+        line-height: 56px;
+        min-width: 120px;
+        padding: 0;
+        border-radius: 4px;
+        margin: 28px 24px 0 0;
+    }
+}
 </style>
