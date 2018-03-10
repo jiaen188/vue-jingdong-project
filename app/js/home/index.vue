a51339102cf4eb10e98ecd6eb584678b09c1951c
jiaen188 <244189981@qq.com>
2018-03-10 17:31:26 +0800

    首页的理财精选部分

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -3,19 +3,25 @@
         <Heador></Heador>
         <Slider></Slider>
         <Novice></Novice>
+        <Borrow></Borrow>
+        <Money></Money>
     </div>
 </template>
 
 <script>
 import Heador from "../public/header.vue";
 import Slider from "./hslider.vue";
-import Novice from "./novice.vue"
+import Novice from "./novice.vue";
+import Borrow from "./borrow.vue";
+import Money from "./money.vue";
 
 export default {
     components: {
         Heador,
         Slider,
-        Novice
+        Novice,
+        Borrow,
+        Money
     }
 }
 </script>
