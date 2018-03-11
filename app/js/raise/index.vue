d2c9894146f51a7be0a6fa58605680a180985fb4
jiaen188 <244189981@qq.com>
2018-03-11 22:21:14 +0800

    众筹页面

---
+++ app/js/raise/index.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        <Daily></Daily>
+        <In></In>
+        <More></More>
+        <Footor cname="mfooter"></Footor>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue";
+import Footor from "../public/footer.vue";
+import Navbar from "../public/navbar.vue";
+import Slider from "./rslider.vue";
+import Daily from "./daily.vue";
+import In from "./in.vue";
+import More from "./more.vue";
+
+export default {
+    components: {
+        Heador,
+        Slider,
+        Daily,
+        In,
+        More,
+        Footor,
+        Navbar
+    }
+}
+</script>
+
+<style lang="scss" module>
+.mfooter{
+    margin-top: 0 !important;
+    ul{
+        li:first-child{
+            display: none;
+        }
+    }
+}
+</style>
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -2,7 +2,8 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from "../money/index.vue"
-import Ious from "../ious/index.vue";
+import Ious from "../ious/index.vue"
+import Raise from "../raise/index.vue"
 import "../../css/reset.scss"
 
 
@@ -22,6 +23,10 @@ export default new Router({
             path: "/ious",
             name: "ious",
             component: Ious,
+        }, {
+            path: "/raise",
+            name: "raise",
+            component: Raise,
         },
     ],
 })
