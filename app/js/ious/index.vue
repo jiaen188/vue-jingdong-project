3182cc0b5a71e484a0639b8d98373b834cad9e7e
jiaen188 <244189981@qq.com>
2018-03-11 18:00:55 +0800

    白条页面

---
+++ app/js/ious/index.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+    <div>
+        <Heador></Heador>
+        <Slider></Slider>
+        <Welfare></Welfare>
+        <Service></Service>
+        <Footor></Footor>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue";
+import Footor from "../public/footer.vue";
+import Navbar from "../public/navbar.vue";
+import Slider from "./islider.vue";
+import Welfare from "./welfare.vue";
+import Service from "./service.vue";
+
+export default {
+    components: {
+        Heador,
+        Slider,
+        Welfare,
+        Service,
+        Footor,
+        Navbar
+    }
+}
+</script>
+
+<style lang="scss" module>
+  
+</style>
